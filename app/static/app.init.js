5473cd9f1a99185af967dda194a025454f25baf6
Josh Ouyang <me@joshoy.org>
2018-06-13 09:06:42 +0800

    code mirror textarea

--- app/static/app.init.js
+++ app/static/app.init.js
@@ -2,6 +2,9 @@
   const appTextArea = document.getElementById('src-code-textarea');
   const editor = CodeMirror.fromTextArea(appTextArea, {
     lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    mode: 'formlang',
   });
   window.editor = editor;
 })();
